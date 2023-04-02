79c85ef974a86eee48312e78906119ca3f2cde2e
sivansuh <sivansuh.is@gmail.com>
2023-04-02 17:29:26 +0300

    recommend page complete

--- src/pages/VideosPage.js
+++ src/pages/VideosPage.js
@@ -1,39 +1,55 @@
+import React, { useEffect } from "react";
 import Search from "../components/Search";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Sidebar from "../components/Sidebar";
-import { useNavigate } from "react-router-dom";
+import RecommendVideo from "../components/RecommendVideo";
+import { fetchVideo, onSelectVideoItem } from "../store/slice/videoSlice";
 
 const VideoPage = () => {
-  const { videoSelect } = useSelector((state) => state.videoSection);
-  const navigate = useNavigate();
+  const { videoSelect, videoList } = useSelector((state) => state.videoSection);
+  const dispatc = useDispatch();
 
+  useEffect(() => {
+    onSubmit("teen wolf");
+  }, []);
+
+  const onSubmit = async (term) => {
+    await dispatc(fetchVideo(term));
+  };
+  const onVideoSelect = (video) => {
+    dispatc(onSelectVideoItem(video));
+    window.scrollTo(10, 0);
+  };
   return (
     <>
-      {videoSelect ? (
-        <div>
-          <Search />
-          <div className="flex px-5">
-            <Sidebar />
-            <div className="w-[600px]">
-              <div className=" h-[400px]">
-                <iframe
-                  title={videoSelect.snippet.title}
-                  src={`https://www.youtube.com/embed/${videoSelect.id.videoId}?autoplay=1`}
-                  allow="autoplay"
-                  className="w-full h-full"
-                ></iframe>
-              </div>
-              <h1 className="my-2">{videoSelect.snippet.title}</h1>
-              <p>{videoSelect.snippet.description}</p>
-              <p>
-                {new Date(videoSelect.snippet.publishTime).toLocaleString()}
-              </p>
+      <div>
+        <Search />
+        <div className="flex px-5">
+          <Sidebar onSubmitProps={onSubmit} />
+          <div className="w-[600px] flex-1">
+            <div className=" h-[600px]">
+              <iframe
+                title={videoSelect.snippet.title}
+                src={`https://www.youtube.com/embed/${videoSelect.id.videoId}?autoplay=1`}
+                allow="autoplay"
+                className="w-full h-full"
+              ></iframe>
             </div>
+            <h1 className="my-2">{videoSelect.snippet.title}</h1>
+            <p>{videoSelect.snippet.description}</p>
+            <p>{new Date(videoSelect.snippet.publishTime).toLocaleString()}</p>
+          </div>
+          <div className="h-[1000px] overflow-x-auto">
+            {videoList?.items.map((video) => (
+              <RecommendVideo
+                video={video}
+                key={video.etag}
+                onVideoSelect={onVideoSelect}
+              />
+            ))}
           </div>
         </div>
-      ) : (
-        navigate("/")
-      )}
+      </div>
     </>
   );
 };
